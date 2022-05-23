0e9e79a6fabc112d9914832d0213c77d295fa18c
barrettwu <barrettwu@tencent.com>
2022-05-23 15:47:13 +0800

    support go install

---
+++ cmd/grr/app/main.go
@@ -0,0 +1,46 @@
+package app
+
+import (
+	log "github.com/sirupsen/logrus"
+
+	"github.com/go-clix/cli"
+	"github.com/grafana/grizzly/pkg/grafana"
+	"github.com/grafana/grizzly/pkg/grizzly"
+)
+
+// Version is the current version of the grr command.
+// To be overwritten at build time
+var Version = "dev"
+
+func Main() {
+
+	rootCmd := &cli.Command{
+		Use:     "grr",
+		Short:   "Grizzly",
+		Version: Version,
+	}
+
+	grizzly.ConfigureProviderRegistry(
+		[]grizzly.Provider{
+			&grafana.Provider{},
+		})
+
+	// workflow commands
+	rootCmd.AddCommand(
+		getCmd(),
+		listCmd(),
+		pullCmd(),
+		showCmd(),
+		diffCmd(),
+		applyCmd(),
+		watchCmd(),
+		exportCmd(),
+		previewCmd(),
+		providersCmd(),
+	)
+
+	// Run!
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/grafana/grizzly/cmd/grr/app"
+
+func main() {
+	app.Main()
+}
